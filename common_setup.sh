6bbb4f96e9e7ef5063982596245a57b8f1697a56
luis <lahoising78@gmail.com>
2021-09-28 23:08:08 -0400

    common setup script support for manjaro

--- common_setup.sh
+++ common_setup.sh
@@ -1,8 +1,53 @@
 #!/bin/bash
 
-# vim plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ "$1" = "-nvim" ]
+then
+	echo "install vim plug for neovim"
+
+	# vim plug - neovim
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+else
+	echo "install vim plug for vim"
+
+	# vim plug
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+function isDistro
+{
+	res=$(cat /etc/os-release | egrep "$1") 
+	if [ -n "$res" ]
+	then
+		echo 1
+		return
+	fi
+
+	echo 0
+}
+
+pm=dnf
+pm_ins=install
+
+packages_to_install="cmake gcc make"
+echo $(isDistro "Manjaro")
+
+if [ $(isDistro "Manjaro") ]
+then
+	echo "manjaro"
+	pm=pacman
+	pm_ins=-S
+	packages_to_install="$packages_to_install python3"
+elif [ $(isDistro "Ubuntu") ]
+then
+	echo "ubuntu"
+	pm=apt
+	packages_to_install="$packages_to_install python3-dev"
+else
+	echo "fedora"
+	packages_to_install="$packages_to_install python3-devel"
+fi
 
 # you complete me essentials
-sudo dnf install cmake gcc-c++ make python3-devel
+sudo $pm $pm_ins $packages_to_install
