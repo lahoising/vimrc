77f1974adc45d709dd17e57cc3c265aafcd0ebc1
luis <lahoising78@gmail.com>
2021-09-30 21:29:40 -0400

    nvim cmp behaves like ycm

--- neovim/include/lsp.lua
+++ neovim/include/lsp.lua
@@ -5,11 +5,8 @@ cmp.setup({
 		['<C-d>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-l>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		})
+		['<TAB>'] = cmp.mapping.select_next_item(),
+		['<s-TAB>'] = cmp.mapping.select_prev_item()
 	},
 	sources = {
 		{ name = 'nvim_lsp' },
