42fd131ca6d0b67c564e728b03c24a8567b2e971
luis <lahoising78@gmail.com>
2021-10-02 11:36:16 -0400

    go definition to <Leader>gd

--- neovim/include/lsp.lua
+++ neovim/include/lsp.lua
@@ -2,8 +2,6 @@ local cmp = require'cmp'
 
 cmp.setup({
 	mapping = {
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<TAB>'] = cmp.mapping.select_next_item(),
 		['<s-TAB>'] = cmp.mapping.select_prev_item()
@@ -17,7 +15,7 @@ cmp.setup({
 local on_attach = function(client,bufnr)
 	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 	local opts = { noremap=true, silent=true }
-	buf_set_keymap('n', '<space>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	buf_set_keymap('n', '<Leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 end
 
 require'lspconfig'.clangd.setup{ on_attach = on_attach }
