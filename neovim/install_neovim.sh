c300324449c095b4709147e31b2e6b7f416d6b5c
Luis Chiang <lahoising78@gmail.com>
2021-10-04 16:13:15 -0500

    install neovim from source if needed

---
+++ neovim/install_neovim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+script_dir=${0%/*}
+source "${script_dir}/../utils.sh"
+
+function install_from_source
+{
+	nvim_repo_dir="./downloaded/neovim-src"
+	git clone git@github.com:neovim/neovim.git "${nvim_repo_dir}"
+	cd "${nvim_repo_dir}"
+
+	nvim_out_dir=~/local/nvim
+	make CMAKE_BUILD_TYPE=Release
+	make CMAKE_INSTALL_PREFIX=${nvim_out_dir} install
+}
+
+if $(isDistro "Manjaro")
+then
+	sudo pacman -S neovim
+elif $(isDistro "Ubuntu")
+then
+	sudo apt update
+	sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl
+
+	install_from_source
+fi
+
